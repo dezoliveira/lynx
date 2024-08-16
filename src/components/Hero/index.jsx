1ab4f16cacebf6ec52d6b1ee9d20fc4a20308990
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-16 11:06:29 -0300

    fix: hero image react path

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,8 +1,10 @@
+import heroImage from '../../logo.png'
+
 const Hero = () => {
   return (
     <div className="relative flex flex-col items-center justify-center gap-4">
       <div className="bg-slate-50 w-[200px] rounded-full">
-        <img src="src\logo.png" className="w-[100%] object-fill"/>
+        <img src={heroImage} className="w-[100%] object-fill"/>
       </div>
       <h1 className="text-3xl text-slate-900 font-semibold">Choose your Lynx</h1>
     </div>
