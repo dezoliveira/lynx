8291c30b62896c7926109508077a02bf853f4377
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-16 13:26:08 -0300

    add: links

--- src/App.jsx
+++ src/App.jsx
@@ -28,56 +28,56 @@ function App() {
           <li className="my-4 bg-slate-50 w-full p-4 border rounded-2xl shadow-2xl">
             <div className="flex items-center justify-center">
               <span className="flex items-center gap-1">
-                <i class="devicon-linkedin-plain colored"></i>
-                <h2>Linkedin</h2>
+                <i class="devicon-linkedin-plain colored text-2xl"></i>
+                <h2 className="text-2xl">Linkedin</h2>
               </span>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={24} icon={LINK}/>
+              <a href="https://www.linkedin.com/in/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                <Player ref={playerRef} size={32} icon={LINK}/>
               </a>
               <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={24} icon={SHARE}/>
+                <Player ref={shareRef} size={32} icon={SHARE}/>
               </a>
             </div>
           </li>
           <li className="my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
             <div className="flex items-center justify-center">
               <span className="flex items-center gap-1">
-                <i class="devicon-github-original colored"></i>
-                <h2>Github</h2>
+                <i class="devicon-github-original colored text-2xl"></i>
+                <h2 className="text-2xl">Github</h2>
               </span>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={24} icon={LINK}/>
+              <a href="https://www.github.com/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                <Player ref={playerRef} size={32} icon={LINK}/>
               </a>
               <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={24} icon={SHARE}/>
+                <Player ref={shareRef} size={32} icon={SHARE}/>
               </a>
             </div>
           </li> 
           <li className="my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
             <div className="flex items-center justify-center">
               <span className="flex items-center gap-1">
-                <i class="devicon-vercel-original colored"></i>
-                <h2>Portfolio</h2>
+                <i class="devicon-vercel-original colored text-2xl"></i>
+                <h2 className="text-2xl">Portfolio</h2>
               </span>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={24} icon={LINK}/>
+              <a href="https://dezoliveira.github.io/myportfolio/" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                <Player ref={playerRef} size={32} icon={LINK}/>
               </a>
               <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={24} icon={SHARE}/>
+                <Player ref={shareRef} size={32} icon={SHARE}/>
               </a>
             </div>
           </li> 
           <li className=" my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
             <div className="flex items-center justify-center">
               <span className="flex items-center gap-1">
-                <i class="devicon-contao-original colored"></i>
-                <h1>Contato</h1>
+                <i class="devicon-contao-original colored text-2xl"></i>
+                <h1 className="text-2xl">Contato</h1>
               </span>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={24} icon={LINK}/>
+              <a href="https://wa.me/5519992282130" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                <Player ref={playerRef} size={32} icon={LINK}/>
               </a>
               <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={24} icon={SHARE}/>
+                <Player ref={shareRef} size={32} icon={SHARE}/>
               </a>
             </div>
           </li> 
