f9b260426a70879feeab28af1bd3d9b367d5a899
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-17 11:11:06 -0300

    style: background and link icons

--- src/App.jsx
+++ src/App.jsx
@@ -26,59 +26,67 @@ function App() {
           <Hero /> 
         <ul className="sm:w-[70vh] w-[35vh] sm:p-4 p-2">
           <li className="my-4 bg-slate-50 w-full p-4 border rounded-2xl shadow-2xl">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-4">
               <span className="flex items-center gap-1">
                 <i class="devicon-linkedin-plain colored text-2xl"></i>
                 <h2 className="text-2xl">Linkedin</h2>
               </span>
-              <a href="https://www.linkedin.com/in/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={32} icon={LINK}/>
-              </a>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={32} icon={SHARE}/>
-              </a>
+              <div className="flex items-center">
+                <a href="https://www.linkedin.com/in/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={playerRef} size={32} icon={LINK}/>
+                </a>
+                <a onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={shareRef} size={32} icon={SHARE}/>
+                </a>
+              </div>
             </div>
           </li>
           <li className="my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-8">
               <span className="flex items-center gap-1">
                 <i class="devicon-github-original colored text-2xl"></i>
                 <h2 className="text-2xl">Github</h2>
               </span>
-              <a href="https://www.github.com/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={32} icon={LINK}/>
-              </a>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={32} icon={SHARE}/>
-              </a>
+              <div className="flex items-center">
+                <a href="https://www.github.com/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={playerRef} size={32} icon={LINK}/>
+                </a>
+                <a onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={shareRef} size={32} icon={SHARE}/>
+                </a>
+              </div>
             </div>
           </li> 
           <li className="my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-4">
               <span className="flex items-center gap-1">
                 <i class="devicon-vercel-original colored text-2xl"></i>
                 <h2 className="text-2xl">Portfolio</h2>
               </span>
-              <a href="https://dezoliveira.github.io/myportfolio/" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={32} icon={LINK}/>
-              </a>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={32} icon={SHARE}/>
-              </a>
+              <div className="flex items-center">
+                <a href="https://dezoliveira.github.io/myportfolio/" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={playerRef} size={32} icon={LINK}/>
+                </a>
+                <a onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={shareRef} size={32} icon={SHARE}/>
+                </a>
+              </div>
             </div>
           </li> 
           <li className=" my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center gap-4">
               <span className="flex items-center gap-1">
                 <i class="devicon-contao-original colored text-2xl"></i>
                 <h1 className="text-2xl">Contato</h1>
               </span>
-              <a href="https://wa.me/5519992282130" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={playerRef} size={32} icon={LINK}/>
-              </a>
-              <a onMouseEnter={playIcons} className="cursor-pointer">
-                <Player ref={shareRef} size={32} icon={SHARE}/>
-              </a>
+              <div className="flex items-center">
+                <a href="https://wa.me/5519992282130" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={playerRef} size={32} icon={LINK}/>
+                </a>
+                <a onMouseEnter={playIcons} className="cursor-pointer">
+                  <Player ref={shareRef} size={32} icon={SHARE}/>
+                </a>
+              </div>
             </div>
           </li> 
         </ul>
