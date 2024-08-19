cd12ec96685468a4045a0225777c37fa7d15d9ad
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-19 12:05:56 -0300

    add: api quest to get links

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,12 @@ function App() {
 
   useEffect(() => {
     getLinks()
-    playIcons()
+    // playIcons()
   }, []);
 
   const getLinks = async () => {
     const { data, error } = await supabase.from("links").select()
     setLinks(data)
-    console.log(data)
   }
 
   const playIcons = () => {
@@ -36,77 +35,44 @@ function App() {
     shareRef.current.playFromBeginning();
   }
 
+  const capitalizeText = (text) => {
+    if (!text)
+      return
+
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
   return (
     <div className="App">
       <div className="relative h-[80vh] sm:w-[80vh] w-[40vh] bg-amber-400 flex flex-col items-center justify-center rounded-2xl shadow-xl border-slate-950">
         <div className='h-[25%] bg-neutral-950 w-full absolute top-0 rounded-t-lg'>
         </div>
-          <Hero /> 
+        <Hero /> 
         <ul className="sm:w-[70vh] w-[35vh] sm:p-4 p-2">
-          <li className="my-4 bg-slate-50 w-full p-4 border rounded-2xl shadow-2xl">
-            <div className="flex items-center justify-center gap-4">
-              <span className="flex items-center gap-1">
-                <i className="devicon-linkedin-plain colored text-2xl"></i>
-                <h2 className="text-2xl">Linkedin</h2>
-              </span>
-              <div className="flex items-center">
-                <a href="https://www.linkedin.com/in/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={playerRef} size={32} icon={LINK}/>
-                </a>
-                <a onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={shareRef} size={32} icon={SHARE}/>
-                </a>
-              </div>
-            </div>
-          </li>
-          <li className="my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
-            <div className="flex items-center justify-center gap-8">
-              <span className="flex items-center gap-1">
-                <i className="devicon-github-original colored text-2xl"></i>
-                <h2 className="text-2xl">Github</h2>
-              </span>
-              <div className="flex items-center">
-                <a href="https://www.github.com/dezoliveira" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={playerRef} size={32} icon={LINK}/>
-                </a>
-                <a onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={shareRef} size={32} icon={SHARE}/>
-                </a>
-              </div>
-            </div>
-          </li> 
-          <li className="my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
-            <div className="flex items-center justify-center gap-4">
-              <span className="flex items-center gap-1">
-                <i className="devicon-vercel-original colored text-2xl"></i>
-                <h2 className="text-2xl">Portfolio</h2>
-              </span>
-              <div className="flex items-center">
-                <a href="https://dezoliveira.github.io/myportfolio/" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={playerRef} size={32} icon={LINK}/>
-                </a>
-                <a onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={shareRef} size={32} icon={SHARE}/>
-                </a>
-              </div>
-            </div>
-          </li> 
-          <li className=" my-4 bg-slate-50 w-full p-4 border rounded-lg shadow-2xl">
-            <div className="flex items-center justify-center gap-4">
-              <span className="flex items-center gap-1">
-                <i className="devicon-contao-original colored text-2xl"></i>
-                <h1 className="text-2xl">Contato</h1>
-              </span>
-              <div className="flex items-center">
-                <a href="https://wa.me/5519992282130" target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={playerRef} size={32} icon={LINK}/>
-                </a>
-                <a onMouseEnter={playIcons} className="cursor-pointer">
-                  <Player ref={shareRef} size={32} icon={SHARE}/>
-                </a>
-              </div>
-            </div>
-          </li> 
+          {
+            links !== undefined ? 
+              links.map((link) => (
+                <div key={link.id}>
+                  <li className="my-4 bg-slate-50 w-full p-4 border rounded-2xl shadow-2xl">
+                    <div className={`flex items-center justify-center ${link.title === 'github' ? 'gap-8' : 'gap-4'}`}>
+                      <span className="flex items-center gap-1">
+                        <i className={`${link.icon} colored text-2xl`}></i>
+                        <h2 className="text-2xl">{capitalizeText(link.title)}</h2>
+                      </span>
+                      <div className="flex items-center">
+                        <a href={link.url} target="_blank" onMouseEnter={playIcons} className="cursor-pointer">
+                          <Player ref={playerRef} size={32} icon={LINK}/>
+                        </a>
+                        <a onMouseEnter={playIcons} className="cursor-pointer">
+                          <Player ref={shareRef} size={32} icon={SHARE}/>
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                </div>
+              )) : <></>
+            }
+            
         </ul>
       </div>
     </div>
