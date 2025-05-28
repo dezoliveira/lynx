e14390849980003708923f97ca0ba981a5e2282b
Andres Oliveira <andresoliveira@protonmail.com>
2025-05-27 22:50:12 -0300

    style: hero + Link Container

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,11 +2,10 @@ import heroImage from '../../logo.png'
 
 const Hero = () => {
   return (
-    <div className="relative top-0 flex flex-col items-center justify-center gap-4">
+    <div className="absolute top-[60px] flex flex-col items-center justify-center gap-4">
       <div className="bg-slate-50 w-[200px] rounded-full">
         <img src={heroImage} className="w-[100%] object-fill"/>
       </div>
-      <h1 className="text-2xl sm:text-3xl text-slate-900 font-semibold">A Lynx te guiará até mim</h1>
     </div>
   )
 }
