66abcd0b1f8470c08a4f8cfd748f3838875cdb30
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-02 15:14:10 -0300

    add: Info on admin login

--- src/pages/admin/Dashboard/index.jsx
+++ src/pages/admin/Dashboard/index.jsx
@@ -1,14 +1,60 @@
+import { collection, getDocs } from "firebase/firestore"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { db } from "../../../lib/firebaseConfig"
 
 export default function Dashboard() {
-  console.log('dashboard')
+  const [links, setLinks] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [lastDate, setLastDate] = useState(null)
 
+  useEffect(() => {
+    const fetchLinks = async () => {
+      setLoading(true)
+
+      try {
+        const query = await getDocs(collection(db, "links"))
+        const data = query.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }))
+
+        setLinks(data)
+        getDateInfo(data)
+        setLoading(false)
+
+      } catch (error) {
+        console.error("Erro ao buscar links", error)
+        setError("Erro ao carregar as informações de usuário" + Error)
+        setLoading(false)
+      }
+    }
+
+    const getDateInfo = (data) => {
+      const getCreatedAt = data.map(link => link.createdAt)
+      const orderedCreatedAt = getCreatedAt.sort((a, b) => a - b)
+      const latest = orderedCreatedAt[orderedCreatedAt.length - 1]
+
+      if (latest) {
+        const lastDateString = new Date(latest)
+        const lastDateTimeString = `${lastDateString.toLocaleDateString()} ás ${lastDateString.toLocaleTimeString()}`
+        setLastDate(lastDateTimeString)
+      
+      } else {
+        setLastDate("Nenhuma data encontrada")
+      }
+    }
+
+    fetchLinks()
+  }, [lastDate])
+  
   return (
     <div className="flex flex-col gap-[25px] h-full w-full p-[50px]">
       <h1>Bem-vindo, Admin!</h1>
       <div className="flex flex-col">
-        <span>Você tem 8 links ativos.</span>
-        <span>Última edição feita há 3 dias.</span>
+        <span>Você tem {links?.length} links ativos.</span>
+        <span>Última edição feita {lastDate}.</span>
       </div>
       <div className="flex gap-[10px] items-center justify-center my-[25px]">
         <Link to="/admin/edit">
