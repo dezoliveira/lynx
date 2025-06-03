60f63d93eef2b5fb56ccd337bfbea4e2aea0f9f3
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-03 08:40:59 -0300

    style: navbar update

--- src/components/AdminLayout/index.jsx
+++ src/components/AdminLayout/index.jsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router-dom";
 export default function AdminLayout() {
   return (
     <div className="flex flex-col h-full mt-[50px]">
-      <header className="flex text-slate-50 bg-slate-900 rounded-lg p-4">
+      <header className="flex justify-center text-slate-50 bg-slate-900 rounded-lg p-4">
         <nav className="gap-[15px] flex">
-          <Link to="/" className="hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Home</Link>|
-          <Link to="/admin/edit" className="hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Editar Links</Link>|
-          <Link className="hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]"> Logout</Link>
+          <Link to="/" className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Home</Link>
+          <Link to="/admin/edit" className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Editar Links</Link>
+          <Link className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]"> Logout</Link>
         </nav>
       </header>
       <main className="p-4">
