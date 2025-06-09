26a59b2b0cdd41aa12f11e5c489df924077f287a
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-09 18:45:51 -0300

    fix: admin layout fix

--- src/components/AdminLayout/index.jsx
+++ src/components/AdminLayout/index.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
   return (
-    <div className="flex flex-col h-full mt-[50px]">
+    <div className="flex flex-col h-full">
       <header className="flex justify-center text-slate-50 bg-slate-900 rounded-lg p-4">
         <nav className="gap-[15px] flex">
           <Link to="/" className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Home</Link>
@@ -11,7 +11,7 @@ export default function AdminLayout() {
           <Link className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]"> Logout</Link>
         </nav>
       </header>
-      <main className="p-4">
+      <main>
         <Outlet />
       </main>
     </div>
