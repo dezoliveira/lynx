0ff6bbedfa21209e6f8553ae1555f37a7306f1f5
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-10 18:23:06 -0300

    style: css modules -> Link List, loading & navigate Button

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,7 +1,9 @@
+import styles from './styles.module.css'
+
 const Loading = () => {
   return (
     <>
-      <div className="flex items-center justify-center">
+      <div className={styles.loadingContainer}>
         <span className="loader"></span>
       </div>
     </>
