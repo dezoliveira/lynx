94d4aa9dcf4b44aa303a9484cee25c5b2b9ad8fa
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-10 17:20:12 -0300

    style: modules css -> linkContainer

--- src/components/LinkContainer/index.jsx
+++ src/components/LinkContainer/index.jsx
@@ -1,7 +1,9 @@
+import styles from './styles.module.css'
+
 export default function LinkContainer({ children }) {
   return (
-    <div className="relative sm:pt-[250px] pt-[80px] h-[100vh] sm:h-[90vh] sm:w-[60vh] w-[100vh] bg-amber-400 flex flex-col items-center justify-center rounded-2xl shadow-xl border-slate-950">
-      <div className='h-[180px] bg-neutral-950 w-full absolute top-0 rounded-t-lg'>
+    <div className={styles.linkContainer}>
+      <div className={styles.linkContent}>
 
       </div>
       { children }
