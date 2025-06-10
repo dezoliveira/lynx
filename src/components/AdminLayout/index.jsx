93a0c98c3c10d85f4111396ce5efd84d9c413fb7
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-10 16:15:43 -0300

    style:  css modules -> admin layout

--- src/components/AdminLayout/index.jsx
+++ src/components/AdminLayout/index.jsx
@@ -1,14 +1,18 @@
+// Components
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
+// Styles
+import styles from './styles.module.css'
+
 export default function AdminLayout() {
   return (
-    <div className="flex flex-col h-full">
-      <header className="flex justify-center text-slate-50 bg-slate-900 rounded-lg p-4">
-        <nav className="gap-[15px] flex">
-          <Link to="/" className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Home</Link>
-          <Link to="/admin/edit" className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]">Editar Links</Link>
-          <Link className="bg-slate-950 py-[8px] px-[12px] rounded-lg hover:opacity-[.9] hover:text-amber-400 hover:scale-[1.03] transition-all duration-[.2s]"> Logout</Link>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <Link to="/">Home</Link>
+          <Link to="/admin/edit">Editar</Link>
+          <Link> Logout</Link>
         </nav>
       </header>
       <main>
