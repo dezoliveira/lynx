ea550d1d8004289974f1e9d1a854ab00e5c4acbc
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-12 12:01:24 -0300

    add: use login hook

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { auth } from "../lib/firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export default function useLogin() {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const login = async (email, password) => {
+    setError("")
+    setLoading(true)
+
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth, login, password)
+      const token = await userCredentials.user.getIdToken()
+
+      setLoading(false)
+      return { success: true }
+      
+    } catch (error) {
+      return { success: false, error: error.code || error.message }      
+    }
+  }
+
+  return { login, error, loading }
+}
--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
@@ -12,6 +13,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+
+const auth = getAuth()
 const db = getFirestore(app)
 
-export { db }
+export { auth, db }
