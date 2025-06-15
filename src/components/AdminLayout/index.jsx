12c9bce4f557c8ad485b48b8730c6a1d657d1dd8
Andres Oliveira <andresoliveira@protonmail.com>
2025-06-15 19:25:01 -0300

    style: spacing adjusts

--- src/components/AdminLayout/index.jsx
+++ src/components/AdminLayout/index.jsx
@@ -39,7 +39,7 @@ export default function AdminLayout() {
     <div className={styles.adminContainer}>
       <header className={styles.header}>
         <nav className={styles.navbar}>
-          <Link to="/">Home</Link>
+          <Link to="/admin">Home</Link>
           <Link to="/admin/edit">Editar</Link>
           <Link onClick={handleLogout}> Logout</Link>
         </nav>
